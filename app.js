dd479a1bc7e220695a97fedf7bec078944bf6130
acopperlily <acopperlily@yahoo.com>
2022-08-18 05:21:25 -0400

    Add more files

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db');
+const { default: mongoose } = require('mongoose');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -34,7 +36,10 @@ app.set('view engine', '.hbs');
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI
+  })
 }));
 
 // Passport middleware
--- routes/auth.js
+++ routes/auth.js
@@ -12,4 +12,16 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
   res.redirect('/dashboard');
 });
 
+// @desc    Logout User
+// @route   /auth/logout
+// Change: Passport 0.6 requires logout to be async
+router.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
